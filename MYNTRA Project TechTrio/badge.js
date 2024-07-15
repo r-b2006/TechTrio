1c6dd1d52891eb09ab4ef3541c66fb774b5092fe
r-b2006 <roopikabansal2006@gmail.com>
2024-07-15 10:05:42 +0530

    Update badge.js

--- MYNTRA Project TechTrio/badge.js
+++ MYNTRA Project TechTrio/badge.js
@@ -9,3 +9,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const username3 = urlParams.get('username');
 const p = document.querySelector(".fullp");
 p.innerText = `HI ${username3}!`;
+
+document.getElementById('shares').addEventListener('click', function() {
+  window.location.href = 'http://localhost:3000'; // Replace with your React app URL
+});
